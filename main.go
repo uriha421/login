68385cb26af4b5c1e3c50e82e877ceae56e93f90
uriha421 <uriha421@gmail.com>
2018-09-24 18:51:54 +0900

    fix bugs

--- main.go
+++ main.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/login.html"))
+	t.Execute(w, nil)
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	user, err := getUserByEmail(r.PostFormValue("email"))
+
+	if user.Password != r.PostFormValue("password") {
+		fmt.Println("invalid password")
+	}
+
+	session, err := user.createSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cookie := http.Cookie{
+		Name:     "_cookie",
+		Value:    session.Uuid,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+
+	http.Redirect(w, r, "/", 302)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := session(w, r)
+	if err != nil {
+		fmt.Println("error")
+		http.Redirect(w, r, "/login", 302)
+	}
+
+	fmt.Println(session)
+
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	t.Execute(w, nil)
+}
 
 func main() {
 	server := http.Server{
 		Addr: "localhost:8080",
 	}
+
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/auth", authHandler)
-	http.HandleFunc("/signup", signupHandler)
-	http.HandleFunc("/register", registerHandler)
+
 	server.ListenAndServe()
 }
--- setup.sql
+++ setup.sql
@@ -1,19 +1,21 @@
-drop table if exists users;
-drop table if exists sessions cascade;
+drop table if exists users cascade;
+drop table if exists sessions;
 
 create table users (
   id         serial primary key,
-  uuid       varchar(64) not null unique,
-  name       varchar(255),
+  uuid varchar(64) not null unique,
+  name varchar(255) not null,
   email      varchar(255) not null unique,
   password   varchar(255) not null,
+  admin smallint not null,
   created_at timestamp not null
 );
 
 create table sessions (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  email      varchar(255),
-  user_id    integer references users(id),
-  created_at timestamp not null
+    id	    serial primary key,
+    uuid    varchar(64) not null unique,
+    user_id integer references users(id),
+    admin smallint not null,
+    created_at timestamp not null
 );
+
